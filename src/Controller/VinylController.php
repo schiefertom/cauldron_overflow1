626e0e0f23ea395b4b7313f59330ff8e5f50ba7a
tsie <schiefertom@gmail.com>
2022-04-25 14:32:28 +0200

    added twig to the project and created first twig template

--- src/Controller/VinylController.php
+++ src/Controller/VinylController.php
@@ -3,17 +3,30 @@
 namespace App\Controller;
 
 use JetBrains\PhpStorm\NoReturn;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use function Symfony\Component\String\u;
 
-class VinylController
+class VinylController extends AbstractController
 {
 
     #[Route('/', name: 'homepage')]
     public function homepage(): Response
     {
-        return new Response('Title: DJ Toms Mega Collection');
+        $tracks = [
+          ['songName' => 'Gangsta\'s Paradise', 'artist' => 'Coolio'],
+          ['songName' => 'Sinnermann', 'artist' => 'Prelude Mix'],
+          ['songName' => 'Grandma', 'artist' => 'Dole & Korn'],
+          ['songName' => 'Black Betty', 'artist' => 'Ram Jam'],
+          ['songName' => 'Das Elfte Gebot', 'artist' => 'Feuerschwanz']
+        ];
+
+        //first parameter is the name of the twig template, second parameter is an array of variables passed to the twig template
+        return $this->render('vinyl/homepage.html.twig', [
+            'title' => "DJ Toms Mega Collection",
+            'tracks' => $tracks
+        ]);
     }
 
     #[Route('/browse/{slug}', name: 'browse')]
